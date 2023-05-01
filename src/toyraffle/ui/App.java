422ae3d75ca8d30feea7fdf83cff0330c1d33079
Nadezhda26m <nadezhda-26m@mail.ru>
2023-05-01 16:08:58 +0300

    add work with empty DB

--- src/toyraffle/ui/App.java
+++ src/toyraffle/ui/App.java
@@ -25,29 +25,77 @@ public class App {
                 "Увеличить количество игрушек",
                 "Уменьшить количество игрушек",
                 "Удалить игрушку",
-                "Посмотреть количество призов"
+                "Посмотреть количество призов",
+                "Посмотреть список игрушек",
+                "Выход из настроек"
         };
+        appMenu(p, commandsRaffle, commandsSettings);
+    }
+
+    private static void settings(Presenter p, String[] commandsSettings) {
+        int command;
+        boolean flag = true;
+        while (flag) {
+            p.view.printCommands(commandsSettings);
+            command = p.view.getIndexCommand(commandsSettings);
+            switch (command) {
+                case 0 -> p.addToy();
+                case 1 -> {
+                    if (checkDB(p)) p.changeWeightToy();
+                }
+                case 2 -> {
+                    if (checkDB(p)) p.increaseCountToy();
+                }
+                case 3 -> {
+                    if (checkDB(p)) p.decreaseCountToy();
+                }
+                case 4 -> {
+                    if (checkDB(p)) p.delToy();
+                }
+                case 5 -> p.showTotalCountToys();
+                case 6 -> {
+                    if (checkDB(p)) p.showPresents();
+                }
+                case 7 -> flag = false;
+            }
+        }
+    }
+
+    private static void appMenu(Presenter p, String[] commandsRaffle,
+                                String[] commandsSettings) {
         int command;
         boolean flag = true;
         p.view.printText("Розыгрыш игрушек в магазине детских товаров");
         while (flag) {
             p.view.printCommands(commandsRaffle);
             command = p.view.getIndexCommand(commandsRaffle);
-            if (command == 0) p.getRandomPrize();
-            else if (command == 1) p.givePrize();
-            else if (command == 2) {
-                p.view.printCommands(commandsSettings);
-                command = p.view.getIndexCommand(commandsSettings);
-                switch (command){
-                    case 0 -> p.addToy();
-                    case 1 -> p.changeWeightToy();
-                    case 2 -> p.increaseCountToy();
-                    case 3 -> p.decreaseCountToy();
-                    case 4 -> p.delToy();
-                    case 5 -> p.showTotalCountToys();
+            switch (command) {
+                case 0 -> {
+                    if (p.isEmptyDB()) {
+                        p.view.printText("Подарки не найдены. " +
+                                "Зайдите в настройки и добавьте игрушки.");
+                    } else p.getRandomPrize();
                 }
-                System.out.println();
-            } else flag = false;
+                case 1 -> {
+                    if (p.isEmptyLog()) {
+                        p.view.printText("Все призы разобрали)");
+                    } else p.givePrize();
+                }
+                case 2 -> settings(p, commandsSettings);
+                case 3 -> {
+                    flag = false;
+                    p.view.printText("\nВыход из программы");
+                }
+            }
         }
+    }
+
+    private static boolean checkDB(Presenter p) {
+        if (p.isEmptyDB()) {
+            p.view.printText("Подарки не найдены. " +
+                    "Добавьте игрушку в настройках.");
+            return false;
+        }
+        return true;
     }
 }
